d5f3db3e0da9b98f27698a434858a0b07d79380f
lenarus <86879525+lenarus@users.noreply.github.com>
2024-07-20 17:06:38 +0500

    Update get_current_keyboard_layout.py

--- scripts/get_current_keyboard_layout.py
+++ scripts/get_current_keyboard_layout.py
@@ -1,13 +1,16 @@
 import sys
+import os
 
 
 def main():
+    layout_text = os.popen("hyprctl devices").read().split()
     text = []
-    for i in range(1, len(sys.argv)):
-        if sys.argv[i] == "yes":
+    for i in range(1, len(layout_text)):
+        if layout_text[i] == "yes":
             break
-        text.append(sys.argv[i])
+        text.append(layout_text[i])
     print(text[len(text) - text[::-1].index("keymap:")])
+
 
 if __name__ == "__main__":
     main()
